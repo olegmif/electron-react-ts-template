a0ec06d0f2e61d65a534aae5c8ff8022c9b5fa19
Мифтахов Олег <olegmif@gmail.com>
2024-11-01 19:55:25 +0500

    Simple event validation

--- src/electron/pathResolver.ts
+++ src/electron/pathResolver.ts
@@ -5,3 +5,7 @@ import {app} from "electron";
 export function getPreloadPath() {
     return path.join( app.getAppPath(), isDev() ? '.' : '..', 'dist-electron/preload.cjs' );
 }
+
+export function getIUPath() {
+    return path.join(app.getAppPath() + '/dist-react/index.html')
+}
--- src/electron/util.ts
+++ src/electron/util.ts
@@ -1,14 +1,29 @@
 import {ipcMain} from "electron";
 import WebContents = Electron.WebContents;
+import {getIUPath} from "./pathResolver.js";
+import WebFrameMain = Electron.WebFrameMain;
+import {pathToFileURL} from 'url';
 
 export function isDev(): boolean {
     return process.env.NODE_ENV === 'development';
 }
 
 export function ipcMainHandle<Key extends keyof EventPayloadMapping>(key: Key, handler: () => EventPayloadMapping[Key]) {
-    ipcMain.handle(key, () => handler());
+    ipcMain.handle(key, (event) => {
+        validateEventFrame(event.senderFrame)
+        return handler()
+    });
 }
 
 export function ipcWebContentsSend<Key extends keyof EventPayloadMapping>(key: Key, webContents: WebContents, payload: EventPayloadMapping[Key]) {
     webContents.send(key, payload);
+}
+
+export function validateEventFrame(frame: WebFrameMain){
+    if (isDev() && new URL(frame.url).host === 'localhost:5123') {
+        return;
+    }
+    if (frame.url !== pathToFileURL(getIUPath()).toString()) {
+        throw new Error('Malicious event');
+    }
 }
